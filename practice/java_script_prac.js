7702126217a856eaafa5ec0b89795780d71fbe90
Kevin Choy <kevinchoy22@gmail.com>
2018-12-05 15:27:25 -0800

    zig zag pattern

--- practice/java_script_prac.js
+++ practice/java_script_prac.js
@@ -45,3 +45,22 @@ let bar = foo(2);
 bar(3); // 7
 bar(5); // 12
 bar(10); // 23
+
+// zig zag pattern 
+var convert = function(s, numRows) {
+  var result = [];
+  var step = 1, index = 0;
+  for(var i = 0; i < s.length; i++){
+      if(result[index] === undefined){//'undefined' will be put into string without this
+          result[index] = '';
+      }
+      result[index] += s[i];
+      if(index === 0){
+          step = 1;
+      }else if(index === numRows - 1){
+          step = -1;
+      }
+      index += step;
+  }
+  return result.join('');
+};
