13ae5244495e28d1b5b01770f787cd4131249a0d
Kevin Choy <kevinchoy22@gmail.com>
2018-12-17 12:11:42 -0800

    bsearch

--- javascript/introtojs/phase_4_recursion.js
+++ javascript/introtojs/phase_4_recursion.js
@@ -19,4 +19,40 @@ function sumRec(arr) {
   return sumRec(arr) + last;
 }
 
-console.log(sumRec([1,2,3,4,5]));
+// console.log(sumRec([1,2,3,4,5]));
+
+function exponent(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  return base * exponent(base, exp-1);
+}
+
+// console.log(exponent(2,4));
+
+function deepDup(arr) {
+  if (!(arr instanceof Array)) {
+    return arr;
+  }
+
+  return arr.map((el) => {
+    return deepDup(el);
+  }); 
+}
+
+function bsearch(arr, target) {
+  if (arr.length === 0) {
+    return -1;
+  }
+
+  let mid = Math.floor(arr.length / 2);
+  if (arr[mid] == target) {
+    return mid;
+  } else if (arr[mid] > target) {
+    return bsearch(arr.slice(0,mid), target);
+  } else {
+    return mid + 1 + bsearch(arr.slice(mid+1), target);
+  }
+}
+
+console.log(bsearch([1,2,3,4,5,6,7,8], 7));
